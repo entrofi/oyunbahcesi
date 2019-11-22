fa626e205ad17993127b71935f347b33a3c577cc
Hasan Comak <entrofi@gmail.com>
2019-11-22 16:47:04 +0100

    Introduce job creation script

---
+++ ci_cd/jenkins/configs/jobdsl/create-jobs.groovy
@@ -0,0 +1,38 @@
+
+
+def multiBranchJobs = [
+        [
+                name : 'microservices-sandbox',
+                remote: 'https://github.com/entrofi/microservicesSandbox.git',
+                includes: '*'
+        ],
+        [
+                name : 'spring-sandbox',
+                remote: 'https://github.com/entrofi/spring.git',
+                includes: '*'
+        ]
+]
+
+multiBranchJobs.each {
+    currentJob ->
+        multibranchPipelineJob(currentJob.name) {
+            branchSources {
+                git {
+                    id(UUID.randomUUID().toString())
+                    remote(currentJob.remote)
+                    includes(currentJob.includes)
+                }
+            }
+            factory {
+                workflowBranchProjectFactory {
+                    scriptPath(currentJob.jenkinsFilePath ?: 'Jenkinsfile')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(100)
+                    daysToKeep(10)
+                }
+            }
+        }
+}
